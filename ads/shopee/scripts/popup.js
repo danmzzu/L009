c79020bb98aeb0e0877d8ae96c38b5fc5a3923c6
Daniel Mazzeu <danmzzu@gmail.com>
2025-04-26 16:15:34 -0300

    Upload

--- ads/shopee/scripts/popup.js
+++ ads/shopee/scripts/popup.js
@@ -1,11 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const popupElement = document.getElementById('popup');
     const closeButton = document.getElementById('popup-close');
-  
+    let isPopupVisible = true;
+
+    function hidePopup() {
+        if (popupElement && isPopupVisible) {
+            popupElement.style.display = 'none';
+            isPopupVisible = false;
+            setTimeout(showPopup, 300000);
+        }
+    }
+
+    function showPopup() {
+        if (popupElement && !isPopupVisible) {
+            popupElement.style.display = 'block';
+            isPopupVisible = true;
+        }
+    }
+
     if (popupElement && closeButton) {
         closeButton.addEventListener('click', function(event) {
             event.preventDefault();
-            popupElement.style.display = 'none';
+            hidePopup();
         });
+        showPopup();
     }
 });
