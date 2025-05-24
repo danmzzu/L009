49b571965a06f2c72ffda3e512f8cb569a3036fb
L009 <danmzzu@gmail.com>
2025-05-24 20:58:10 -0300

    Upload

--- scripts/effects.js
+++ scripts/effects.js
@@ -1,37 +1,6 @@
-// Card Feedback Stars
-document.addEventListener('DOMContentLoaded', function() {
-    const starContainers = document.querySelectorAll('.card-feedback-stars');
-
-    starContainers.forEach(container => {
-        const stars = container.querySelectorAll('i');
-        const animationStyle = stars[0].style.animation;
-
-        function disableAnimation() {
-            stars.forEach(star => {
-                star.style.animation = 'none';
-            });
-        }
-
-        function enableAnimation() {
-            stars.forEach((star, index) => {
-                star.style.animation = animationStyle;
-            });
-        }
-
-        function triggerAnimationCycle() {
-            enableAnimation();
-            setTimeout(() => {
-                disableAnimation();
-                setTimeout(triggerAnimationCycle, 2000);
-            }, (0.1 * stars.length * 1000) + 600);
-        }
-
-        triggerAnimationCycle();
-    });
-});
-
+// Fade In Cards
 document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
+    const cards = document.querySelectorAll('.card, .divider, footer, header');
     let lastScrollY = window.scrollY;
 
     const observer = new IntersectionObserver(entries => {
