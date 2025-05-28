40000c2f6cb4601fcb668c2f550f893899a2de6b
L009 <danmzzu@gmail.com>
2025-05-28 09:36:44 -0300

    Upload

--- scripts/effects.js
+++ scripts/effects.js
@@ -28,26 +28,3 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(card);
     });
 });
-
-// Internal # Links Middle of Screen
-document.addEventListener('DOMContentLoaded', () => {
-    const internalLinks = document.querySelectorAll('a[href^="#"]');
-
-    internalLinks.forEach(link => {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-
-            if (targetElement) {
-                targetElement.scrollIntoView({
-                    behavior: 'smooth',
-                    block: 'center'
-                });
-
-                window.history.pushState(null, '', targetId);
-            }
-        });
-    });
-});
